1776dd2245a49a4464f3191100f640766537a947
vagrant <vagrant@kellogg-dev-box>
2014-04-21 14:13:19 +0000

    started books model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Book.create(:title => "A Brief History of Time", :author => "Stephen Hawking")
+Book.create(:title => "Alice in Wonderland", :author => "Lewis Carroll")
+Book.create(:title => "The Lord Of The Rings", :author => "J.R.R. Tolkien")
+Book.create(:title => "The Hobbit", :author => "J.R.R. Tolkien")
