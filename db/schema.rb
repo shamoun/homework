93c806a4e81666a9ced76d9f3610897264f9f41a
vagrant <vagrant@kellogg-dev-box>
2014-05-09 01:50:08 +0000

    loaded book excel

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140421141836) do
+ActiveRecord::Schema.define(version: 20140509013909) do
 
   create_table "books", force: true do |t|
-    t.string "title"
-    t.string "author"
+    t.string  "title"
+    t.string  "author"
+    t.integer "price"
+  end
+
+  create_table "reviews", force: true do |t|
+    t.string  "title"
+    t.integer "rating"
+    t.string  "review"
   end
 
 end
